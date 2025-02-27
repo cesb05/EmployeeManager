3ac99e3cbdf87a079bbe242195f0468ff506b313
César <62319817+cesb05@users.noreply.github.com>
2025-02-27 00:32:49 -0600

    Creación del API REST - Se crearon los diferentes endpoints solicitados. - Se creó el modelo Employee y se configuró el DbContext. - Se configuró Swagger para realizar pruebas de funcionamiento. - Se añadieron validaciones básicas al modelo.

---
+++ EmployeeAPI/Context/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using EmployeeAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeAPI.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){}
+
+        //Modelo a utilizar para la tabla Empleados
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ EmployeeAPI/Models/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmployeeAPI.Models
+{
+    [Table("Empleado")]
+    public class Employee
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo 'Nombre' es obligatorio.")]
+        [StringLength(100, ErrorMessage = "El Nombre no puede tener más de 100 caracteres.")]
+        public required string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El campo 'Apellido' es obligatorio.")]
+        [StringLength(100, ErrorMessage = "El Apellido no puede tener más de 100 caracteres.")]
+        public required string Apellido { get; set; }
+
+        [Required(ErrorMessage = "El campo 'Correo' es obligatorio.")]
+        [EmailAddress(ErrorMessage = "El campo Email no es una dirección de correo válida.")]
+        [StringLength(100, ErrorMessage = "El Email no puede tener más de 100 caracteres.")]
+        public required string Email { get; set; }
+
+        [StringLength(20, ErrorMessage = "El Teléfono no puede tener más de 20 caracteres.")]
+        public string? Telefono { get; set; }
+
+        [Required(ErrorMessage = "El campo 'Salario' es obligatorio.")]
+        [Range(0, double.MaxValue, ErrorMessage = "El Salario debe ser un valor positivo.")]
+        public decimal Salario { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime FechaIngreso { get; set; }
+    }
+}
